0ca39b8a301cc3574b57716d1fbb8e54c7d06567
TRAINER-HP\Brandon Rodriguez <bwrodriguez@student.fullsail.edu>
2018-09-20 22:51:20 -0400

    Cloud Animations and Wave animations

---
+++ ICSMNV1.6/MinkGradProject/Assets/ScrollingTexture.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScrollingTexture : MonoBehaviour {
+	public float ScrollX = 0.05f;
+	bool Negative = false;
+	public Image Wave;
+	// Use this for initialization
+	void Start () {
+		if (Negative == true)
+			ScrollX = -ScrollX;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
