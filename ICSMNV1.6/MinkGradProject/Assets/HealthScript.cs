99ae9dd3475b1f155f31b8c85dc7fe80c789e848
TRAINER-HP\Brandon Rodriguez <bwrodriguez@student.fullsail.edu>
2018-09-30 14:05:21 -0400

    Fixed issue with health

--- ICSMNV1.6/MinkGradProject/Assets/HealthScript.cs
+++ ICSMNV1.6/MinkGradProject/Assets/HealthScript.cs
@@ -3,27 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class HealthScript : MonoBehaviour {
-	public int HealthMax = 0;
-	public int HealthTotal = 0;
+	public float HealthMax = 0;
+	public float HealthTotal = 0;
 	public int HealthCurrent = 0;
-	public Slider HealthBar;
+	public GameObject HealthBar;
 	// Use this for initialization
 	void Start () {
 		
 	}
-	public void SetHealth (int i) {
-		HealthMax = i;
-		HealthBar
-		HealthTotal = i;
+	public void SetHealth (float i) {
+		HealthMax = (float) i;
+        HealthBar.GetComponent<Slider>().value = 1;
+		HealthTotal = (float) i;
 	}
 	public int LoseHealth () {
 
 		HealthTotal--;
-
-
-		return HealthTotal;
-	}
+        HealthBar.GetComponent<Slider>().value = HealthTotal /HealthMax;
+		return (int) HealthTotal;
+		}
 	// Update is called once per frame
 	void Update () {
 		
